77b2ddb0c2f55ec6211aed0d9ce1059adc2771b9
Akkkkiiii <98087722+Akkkkiiii@users.noreply.github.com>
2023-04-18 22:22:14 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 
-const socket = io('http://localhost:8000');
+const socket = io.connect("/");
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp'); 
